fe783a8335bdc3448486774a31fed065c99331b2
zaidmehdi <mehdizaid02@gmail.com>
2024-05-11 19:59:48 +0100

    test /register with valid data and remove tested record from mongodb

---
+++ src/tests/endpoints_tests.py
@@ -0,0 +1,55 @@
+import random
+import string
+import sys
+import unittest
+
+sys.path.append("/home/mehdi/Documents/TVB/moda-backend/src")
+from main import app, db
+
+
+
+class TestRegisterEndpoint(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.collection = db.users
+
+
+    def _generate_random_string(self, length=16):
+        """Generate a random string of letters and digits"""
+
+        characters = string.ascii_letters + string.digits
+
+        return ''.join(random.choice(characters) for _ in range(length))
+
+
+    def _delete_from_mongodb(self, username):
+        """Delete the user from mongodb (after testing registration)
+        because it is not rolled back automatically by flask, unlike sqlite"""
+        
+        self.collection.delete_one({"_id": username})
+
+
+    def test_register_success(self):
+        """Test registration with valid data"""
+        
+        username = self._generate_random_string()
+        password = self._generate_random_string()
+
+        data = {
+            "username": username,
+            "password": password,
+            "gender": "male"
+        }
+
+        response = self.app.post("/register", json=data)
+        self._delete_from_mongodb(username)
+        
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.json["success"])
+        self.assertEqual(response.json["message"], "User successfully registered")
+        self.assertEqual(response.json["user_id"], username)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
