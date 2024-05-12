dce5d0fbd46de7bf6b46e7f6165cc757158dbd1f
zaidmehdi <mehdizaid02@gmail.com>
2024-05-12 23:09:28 +0100

    test_register_success

---
+++ tests/auth_tests.py
@@ -0,0 +1,87 @@
+import os
+import random
+import string
+import sys
+import unittest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.__init__ import create_app
+from src.routes.auth import auth_bp
+
+
+class TestAuthentication(unittest.TestCase):
+    def setUp(self):
+        """Set up the app for testing environment"""
+
+        self.app = create_app(config_name="testing").test_client()
+        self.app_context = create_app(config_name="testing").app_context()
+        self.app_context.push()
+        self.app.application.register_blueprint(auth_bp)
+        self.collection = self.app.application.db.users
+    
+
+    def tearDown(self):
+        """Clean up after the test"""
+
+        self.app_context.pop()
+
+
+    def _generate_random_string(self, length=16):
+        """Generate a random string of letters and digits"""
+
+        characters = string.ascii_letters + string.digits
+
+        return ''.join(random.choice(characters) for _ in range(length))
+
+
+    def _delete_from_mongodb(self, username):
+        """Delete the user from mongodb (after testing registration)
+        because it is not rolled back automatically by flask, unlike sqlite"""
+        
+        self.collection.delete_one({"_id": username})
+
+    
+    def _send_register_post_request(self, username):
+        """Method to send a post request to the /register endpoint
+        and delete the user's entry from mongodb"""
+
+        password = self._generate_random_string()
+
+        data = {
+            "username": username,
+            "password": password,
+            "gender": "male"
+        }
+
+        response = self.app.post("/register", json=data)
+        self._delete_from_mongodb(username)
+
+        return response
+    
+
+    def _send_login_post_request(self, username, password):
+        """Method to send a post request to the /login endpoint"""
+
+        data = {
+            "username": username,
+            "password": password
+        }
+
+        return self.app.post("/login", json=data)
+    
+
+    def test_register_success(self):
+        """Test registration (/register) with valid data"""
+        
+        username = self._generate_random_string()
+        response = self._send_register_post_request(username)
+        
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.json["success"])
+        self.assertEqual(response.json["message"], "User successfully registered")
+        self.assertEqual(response.json["user_id"], username)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
