1965113c99e2bd14dbcc50bd9f8de0ba3bc3d679
zaidmehdi <mehdizaid02@gmail.com>
2024-05-18 00:04:04 +0100

    update tests to new endpoints

--- tests/auth_tests.py
+++ tests/auth_tests.py
@@ -19,7 +19,7 @@ class TestAuthentication(unittest.TestCase):
         self.collection = self.client.application.db.users
 
         with self.app.app_context():
-            self._send_signup_post_request(username="test_user", password="test_password")
+            self._send_signup_post_request(username="test_user", email="test.email@gmail.com", password="test_password")
     
 
     def tearDown(self):
@@ -29,34 +29,35 @@ class TestAuthentication(unittest.TestCase):
         self.app_context.pop()
 
 
-    def _delete_from_mongodb(self, username):
+    def _delete_from_mongodb(self, email):
         """Delete the user from mongodb (after testing registration)
         because it is not rolled back automatically by flask, unlike sqlite"""
         
-        self.collection.delete_one({"_id": username})
+        self.collection.delete_one({"_id": email})
 
     
-    def _send_signup_post_request(self, username, password):
+    def _send_signup_post_request(self, username, email, password):
         """Method to send a post request to the /signup endpoint
         and delete the user's entry from mongodb"""
 
         data = {
             "username": username,
+            "gender": "male",
+            "email": email,
             "password": password,
-            "gender": "male"
         }
 
         response = self.client.post("/signup", json=data)
-        self._delete_from_mongodb(username)
+        self._delete_from_mongodb(email)
 
         return response
     
 
-    def _send_login_post_request(self, username, password):
+    def _send_login_post_request(self, email, password):
         """Method to send a post request to the /login endpoint"""
 
         data = {
-            "username": username,
+            "email": email,
             "password": password
         }
         
@@ -67,34 +68,36 @@ class TestAuthentication(unittest.TestCase):
         """Test signup (/signup) with valid data"""
         
         username = "new_user"
+        email = "new.user@gmail.com"
         password = "new_password"
-        response = self._send_signup_post_request(username, password)
+        response = self._send_signup_post_request(username, email, password)
         
         self.assertEqual(response.status_code, 201)
         self.assertTrue(response.json["success"])
         self.assertEqual(response.json["message"], "User successfully registered")
-        self.assertEqual(response.json["user_id"], username)
+        self.assertEqual(response.json["username"], username)
 
     
     def test_signup_existing_user(self):
         """Test registration (/signup) with a user that already exists"""
 
         username = "test_user"
+        email = "test.email@gmail.com"
         password = 'test_password'
-        response = self._send_signup_post_request(username, password)
+        response = self._send_signup_post_request(username, email, password)
 
         self.assertEqual(response.status_code, 409)
         self.assertFalse(response.json["success"])
-        self.assertEqual(response.json["message"], "Username already exists")
+        self.assertEqual(response.json["message"], "User already exists")
 
     
     def test_login_success(self):
         """Test the login functionality of existing user"""
 
-        username = "test_user"
+        email = "test.email@gmail.com"
         password = "test_password"
 
-        response = self._send_login_post_request(username, password)
+        response = self._send_login_post_request(email, password)
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.json["success"])
@@ -103,27 +106,28 @@ class TestAuthentication(unittest.TestCase):
     
     def test_login_wrong_password(self):
         """Test the login functionality with a wrong password"""
-        username = "test_user"
+
+        email = "test.email@gmail.com"
         password = "wrong_password"
 
-        response = self._send_login_post_request(username, password)
+        response = self._send_login_post_request(email, password)
 
         self.assertEqual(response.status_code, 401)
         self.assertFalse(response.json["success"])
-        self.assertEqual(response.json["message"], "Invalid username or password")
+        self.assertEqual(response.json["message"], "Invalid email or password")
 
 
     def test_login_inexistant_user(self):
         """Test the login functionality with user that doesn't exist"""
 
-        username = "inexistant_user"
+        email = "inexistant.user@gmail.com"
         password = "test_password"
 
-        response = self._send_login_post_request(username, password)
+        response = self._send_login_post_request(email, password)
 
         self.assertEqual(response.status_code, 401)
         self.assertFalse(response.json["success"])
-        self.assertEqual(response.json["message"], "Invalid username or password")
+        self.assertEqual(response.json["message"], "Invalid email or password")
 
 
 
